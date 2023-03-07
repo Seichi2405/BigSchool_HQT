fc789df50f4607d95342882c9d34e61dada0a51d
Seichi Thanh <seichi2405@gmail.com>
2023-03-07 20:10:36 +0700

    Add project files.

---
+++ BigSchool_HQT/Controllers/CoursesController.cs
@@ -0,0 +1,35 @@
+﻿using BigSchool_HQT.Models;
+using BigSchool_HQT.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool_HQT.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Index()
+        {
+            return View();
+        }
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
